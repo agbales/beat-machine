a2f639840415436235d0ef89886a8a6e87587c7f
Andrew Bales <agbales@gmail.com>
2018-06-20 00:18:13 -0700

    canvas midi viz

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,61 @@
+// Play
 window.addEventListener("keydown", sampleLookup);
 
 function sampleLookup(event) {
   var key = event.key;
   var samples = {
-    "f" : "Snare01.wav",
-    "h" : "Kick01.wav",
-    "g" : "Hat01.wav", 
-    "t" : "Hat02.wav",
-    "r" : "Snare02.wav",
-    "v" : "Snare03.wav"
+    "f" : { audio: "Snare01.wav", key: "f" },
+    "h" : { audio: "Kick01.wav", key: "h" },
+    "g" : { audio: "Hat01.wav", key: "g" },
+    "t" : { audio: "Hat02.wav", key: "t" },
+    "r" : { audio: "Snare02.wav", key: "r" },
+    "v" : { audio: "Snare03.wav", key: "v" }
   }
   var sample = samples[key];
-  play(sample);
+
+  if (sample) {
+    play(sample);
+    add(sample);
+  }
 }
 
 function play(sample) {
-  var source = "./samples/" + sample;
+  var source = "./samples/" + sample.audio;
   var audio = new Audio(source);
   audio.play();
+}
+
+// Visualize
+var blocks = [];
+
+function add(sample) {
+  var block = sample;
+      block.position = {}
+      block.position.left = 400;
+      blocks.push(block);
+}
+
+var t = setInterval(update,1000);
+
+function update() {
+  blocks.forEach(function(b, i) {
+  	blocks[i].position.left =  blocks[i].position.left - 100;
+    if (blocks[i].position.left < 0) {
+      console.log('delete', blocks[i])
+      blocks.splice(i,1);
+    }
+  });
+  console.log(blocks);
+  updateMidiGrid(blocks);
+}
+
+function updateMidiGrid(blocks) {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  blocks.forEach(function(b){
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(b.position.left,0,30,20);
+  });
 }
